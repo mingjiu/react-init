e7d4537d9bce8c8a5cc2ad6ae9bb0927b988771f
mingjiu <hmj0718@163.com>
2019-10-25 10:15:22 +0800

    main scss

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { store } from './store'
 import Test from './pages/Test'
 import Page from './pages/Page'
 
-import './sass/reset.scss'
+import './sass/index.scss'
 
 ReactDOM.render(<Provider store={store}>
   <BrowserRouter>
